bb18436741d55f306c196ccd6da12fbc2860f6ba
Raga2629 <nasaniragamala@gmail.com>
2026-06-21 23:40:50 +0530

    21-06-2026

---
+++ notebook.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int ans=(n*1000)/100;
+	    cout<<ans<<endl;
+	}
+
+}
