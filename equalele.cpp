c39e393996f7351bea1ab12bec066996409aba3e
Raga2629 <nasaniragamala@gmail.com>
2026-06-28 20:29:27 +0530

    28-06-2026

---
+++ equalele.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        int mx=INT_MIN;
+        unordered_map<int,int> freq;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            freq[a[i]]++;
+            mx=max(mx,freq[a[i]]);
+            
+        }
+        
+        if(freq.size()==n) {
+            cout<<n-1<<endl;
+            continue;
+        }
+       
+        cout<<n-mx<<endl;
+        
+    }
+}
